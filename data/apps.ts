a5d4a60c417bbf3d6c1f742227bfd40c3181ac1a
rohid-hub <rohidul209@gmail.com>
2022-01-31 00:02:50 +0600

    Fix

--- data/apps.ts
+++ data/apps.ts
@@ -5,7 +5,7 @@ const apps: AppType[] = [
     id: "1",
     title: "Code Converter",
     description:
-      "A code converter and formatter app. This is a very usefull app if you work with SVG's and React. You can easily convert SVG to JSX or TSX or React Native component, HTML to JSX, JSON to TypeScript Type. More functionality will be comming soon.",
+      "A code converter app. This is a very useful app if you work with SVG's and React. You can easily convert SVG to JSX or TSX or React Native component, HTML to JSX, JSON to TypeScript Type. More functionality will be coming soon.",
     coverImage: {
       url: "/images/thumbnails/code-converter-thumbnail.png",
     },
@@ -16,7 +16,7 @@ const apps: AppType[] = [
     id: "2",
     title: "ColorPkr",
     description:
-      "A color picker app but not just a picker app :). You can pick color edit color, you will get all the available color formats, it provides shades, tints, tones, hues for your color. It also provide color harmonies like Analogous, Complementary, Double Split Complementary, Rectangle, Split Complementary, Tetradic and Triadic. It also provide Tailwind CSS Color palette for your color.",
+      "A color picker app but not just a picker app :). You can pick color edit color, you will get all the available color formats, it provides shades, tints, tones, hues for your color. It also provides color harmonies like Analogous, Complementary, Double Split Complementary, Rectangle, Split Complementary, Tetradic and Triadic. It also provides a Tailwind CSS Color palette for your color.",
     coverImage: {
       url: "/images/thumbnails/colorpkr-thumbnail.png",
     },
